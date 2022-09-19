3a926b3025a2046b79ef13a604284fc9a3ce6faa
William <william.liu2568@gmail.com>
2022-09-19 14:47:28 -0400

    started implementation of app.js for task-app project

---
+++ task-app/src/components/Overview.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Overview(props) {
+    const { tasks } = props;
+    return(
+        <ul>
+            {tasks.map((task) => {
+                return <li>{task.text}</li>
+            })}
+        </ul>
+    )
+}
+
+export default Overview;
